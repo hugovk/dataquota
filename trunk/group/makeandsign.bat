0e059cbc155c19d8bfdd31e733dd2160c4ac2cdd
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2009-01-11 15:32:56 +0000

    * Changed UID from Protected Range to Unprotected Range so it can be self-signed.  * Added self signing .cer and .key and some batch files.  * Incremented version number.

---
+++ trunk/group/makeandsign.bat
@@ -0,0 +1,5 @@
+move    DataQuota.sis  DataQuota.bak.sis
+move    DataQuota.sisx DataQuota.bak.sisx
+makesis DataQuota.pkg  DataQuota.sis
+signsis DataQuota.sis  DataQuota.sisx DataQuota.cer DataQuota.key password
+call DataQuota.sisx
