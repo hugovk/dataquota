31a5461e3b991486485e2ff0c5acb0ff95eed648
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2009-10-12 10:32:05 +0000

    Changed the version number from "1.0.12" to "1.00(12)"

--- trunk/inc/DataQuotaAppView.h
+++ trunk/inc/DataQuotaAppView.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define __DATAQUOTAAPPVIEW_H__
 
 // CONSTANTS
-_LIT(KVersion, "1.0.12");
+_LIT(KVersion, "1.00(12)");
 
 // INCLUDES
 #include <centralrepository.h>
