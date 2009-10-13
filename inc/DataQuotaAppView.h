8af82127d708181fd929db35631048a4a2df0748
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2009-10-13 09:20:23 +0000

    * Incremented version number to 1.14(0)

--- inc/DataQuotaAppView.h
+++ inc/DataQuotaAppView.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define __DATAQUOTAAPPVIEW_H__
 
 // CONSTANTS
-_LIT(KVersion, "1.01(14)");
+_LIT(KVersion, "1.14(0)");
 
 // INCLUDES
 #include <centralrepository.h>
