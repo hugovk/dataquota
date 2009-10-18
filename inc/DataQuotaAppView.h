e2da52a139db731d99c97bdc05627bb8bc1b30ee
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2009-10-18 18:12:21 +0000

    * Added Hungarian, thanks to Thomas Loerinczy * Incremented version number 1.16(0)

--- inc/DataQuotaAppView.h
+++ inc/DataQuotaAppView.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define __DATAQUOTAAPPVIEW_H__
 
 // CONSTANTS
-_LIT(KVersion, "1.15(0)");
+_LIT(KVersion, "1.16(0)");
 
 // INCLUDES
 #include <centralrepository.h>
