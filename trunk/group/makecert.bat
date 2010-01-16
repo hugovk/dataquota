e1e38f9f765e7edbf130d59ea0dcae133eb98897
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2010-01-16 10:46:39 +0000

    * Updated the certificate used for self-signing. The previous was only valid for one year and expired on 11th January 2010. The new one is valid for 10 years and expires 14th January 2020. * Incremented version to 1.22.

--- trunk/group/makecert.bat
+++ trunk/group/makecert.bat
@@ -1,1 +1,1 @@
-makekeys -cert -password password -len 1024 -dname "CN=Data Quota OU=Development OR=DataQuota CO=FI EM=noone@nowhere.com" DataQuota.key DataQuota.cer
+makekeys -cert  -expdays 3650 -password password -len 1024 -dname "CN=Data Quota OU=Development OR=DataQuota CO=FI EM=spam@example.com" DataQuota.key DataQuota.cer
