db75ff15609857202624bf5fa1ee42ab2fd4ed59
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2010-03-15 20:50:11 +0000

    * Incremented version to 1.25 * Added Ukrainian, thanks to Ivan S. Zamaltynov

--- inc/DataQuotaAppView.h
+++ inc/DataQuotaAppView.h
@@ -1,28 +1,29 @@
 /*
-DataQuotaAppView.h
-
 Data Quota for S60 phones.
+http://code.google.com/p/dataquota/
+Copyright (C) 2008, 2009, 2010  Hugo van Kemenade
 
-This program is free software; you can redistribute it and/or
+This file is part of Data Quota.
+
+Data Quota is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+Data Quota is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+along with Data Quota.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef __DATAQUOTAAPPVIEW_H__
 #define __DATAQUOTAAPPVIEW_H__
 
 // CONSTANTS
-_LIT(KVersion, "1.24");
+_LIT(KVersion, "1.25");
 
 // FORWARD DECLARATIONS
 class CAknNavigationControlContainer;
