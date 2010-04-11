1cadb2335cfb83b874c4c26af1241909898cf4d7
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2010-04-11 20:59:38 +0000

    * Incremented version to 1.26 * Added Hebrew, thanks to Guy Oliver * Updated Polish, thanks to misiooooooo

--- inc/DataQuotaAppView.h
+++ inc/DataQuotaAppView.h
@@ -23,7 +23,7 @@ along with Data Quota.  If not, see <http://www.gnu.org/licenses/>.
 #define __DATAQUOTAAPPVIEW_H__
 
 // CONSTANTS
-_LIT(KVersion, "1.25");
+_LIT(KVersion, "1.26");
 
 // FORWARD DECLARATIONS
 class CAknNavigationControlContainer;
