be1e02915d99013b2c83273020cef1c6b0ebba35
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2010-11-22 21:46:55 +0000

    * Incremented version to 1.32. * Added support for Symbian^3 devices. * Added US English, though it's just reusing UK English at the moment. * Added touch support, if you press the data or date bars, it will let you enter a new value; touch elsewhere to refresh. * Fixed a touch screen bug -- didn't redraw after editing the values from the options menu.
    TODO
    * Touch feedback?

--- group/makeandsign.bat
+++ group/makeandsign.bat
@@ -2,4 +2,4 @@ move    DataQuota.sis  DataQuota.bak.sis
 move    DataQuota.sisx DataQuota.bak.sisx
 makesis DataQuota.pkg  DataQuota.sis
 signsis DataQuota.sis  DataQuota.sisx DataQuota.cer DataQuota.key password
-call DataQuota.sisx
+start DataQuota.sisx
