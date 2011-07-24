735a9cd7864948f2740296292f885f6835c529ee
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2011-07-24 20:10:18 +0000

    - Incremented version to 1.38.  - Upgrading: When launching self-signed, will attempt to launch Ovi-signed.  - Upgrading: When exiting Ovi-signed, will attempt to uninstall self-signed.  - Ovi-signed is version 2.x, self-signed is version 1.x.

---
+++ trunk/inc/DataQuotaUids.h
@@ -0,0 +1,30 @@
+/*
+Data Quota for Symbian phones.
+http://code.google.com/p/dataquota/
+Copyright (C) 2011  Hugo van Kemenade
+
+This file is part of Data Quota.
+
+Data Quota is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+Data Quota is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Data Quota.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __DATAQUOTAUIDS_H__
+#define __DATAQUOTAUIDS_H__
+
+#define KUidOviSigned 0x200427F6
+#define KUidSelfSigned 0xA000B6EF
+
+#endif // __DATAQUOTAUIDS_H__
+
+// End of file
