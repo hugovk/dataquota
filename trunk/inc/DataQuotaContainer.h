58a7c334eb7c4407babc3ad988b25558f88b49d6
hugovk <hugovk@d6cce430-cceb-11dd-8ca8-e3ff79f713b6>
2012-06-14 09:15:27 +0000

    - Incremented version to 1.43/2.43.  - Added Macedonian, thanks to Toshe X Chuchanov (TosheX) for the translation.

--- trunk/inc/DataQuotaContainer.h
+++ trunk/inc/DataQuotaContainer.h
@@ -35,9 +35,9 @@ class CRepository;
 
 // CONSTANTS
 #ifdef __OVI_SIGNED__
-_LIT(KVersion, "2.42");
+_LIT(KVersion, "2.43");
 #else
-_LIT(KVersion, "1.42");
+_LIT(KVersion, "1.43");
 #endif
 
 // CLASS DECLARATION
